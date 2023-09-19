ec9be817881fe63f0252df5793e7e00ead208f9a
John Wright <jrite@protonmail.com>
2023-09-19 14:35:18 +0100

    chore: correct release syntax

---
+++ release.config.cjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+/**
+ * @type {import('semantic-release').Options}
+ */
+module.exports = {
+  'branches': [
+    'master'
+  ],
+  'plugins': [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        'message': 'chore(release): ${nextRelease.version} [skip ci]'
+      }
+    ],
+    '@semantic-release/github'
+  ]
+}
