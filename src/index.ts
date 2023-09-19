1972bee5ef73e349032677b8b73c12345887c67f
John Wright <jrite@protonmail.com>
2023-09-19 11:33:46 +0100

    chore: remove built index file from git

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { GoogleAdManager as v202211 } from './api/v202211'
-export { GoogleAdManager as v202302 } from './api/v202302'
-export { GoogleAdManager as v202305 } from './api/v202305'
-export { GoogleAdManager as v202308 } from './api/v202308'
