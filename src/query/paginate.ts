4983f1c39e111e4a93c545a18a09a41f7d77453d
John Wright <jrite@protonmail.com>
2023-10-30 12:30:41 +0000

    fix: pagination
    Make sure pagination ends at some point

--- src/query/paginate.ts
+++ src/query/paginate.ts
@@ -1,17 +1,13 @@
 export async function* paginate<T>(
   executeQuery: (limit: number, offset: number) => Promise<Response<T>>,
   limit: number,
-  offset = 0,
+  offset = 0
 ): AsyncGenerator<T> {
-  while (true) {
-    const response = await executeQuery(limit, offset)
-    if (response.rval?.results?.length) {
-      for (const result of response.rval.results) yield result
-      if (response.rval.results.length < limit) break
-      else yield* paginate(executeQuery, limit, offset + limit)
-    } else {
-      break
-    }
+  const response = await executeQuery(limit, offset)
+  if (response.rval?.results?.length) {
+    for (const result of response.rval.results) yield result
+    if (response.rval.results.length >= limit)
+      yield* paginate(executeQuery, limit, offset + limit)
   }
 }
 
