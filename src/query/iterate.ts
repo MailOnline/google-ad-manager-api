7c298b52ea1c7c77e0a7ad180a1f3d87ee0fe8d7
John Wright <jrite@protonmail.com>
2024-01-24 13:59:21 +0000

    feat: paginate to receive pages of items
    Re-introduce the paginate function that will yield pages of items.
    
    BREAKING CHANGE: iterate() will use paginate under the hood and inherit it's options.

--- src/query/iterate.ts
+++ src/query/iterate.ts
@@ -1,38 +1,13 @@
-import { setTimeout } from 'node:timers/promises'
-import { GetByStatementResponse } from './statement'
-
-export interface IterateOptions<T> {
-  /**
-   * The "LIMIT" passed to queries
-   */
-  querySize?: number
-
-  startingOffset?: number
-
-  /**
-   * If you want to makre sure not to exceed your query quota,
-   * you can apply an interval inbetween each query
-   */
-  interval?: number
-
-  /**
-   * How to execute your query
-   */
-  executeQuery(
-    limit: number,
-    offset: number
-  ):
-    | Promise<GetByStatementResponse<T>>
-    | Promise<[GetByStatementResponse<T>, ...any[]]>
-}
+import { PaginateOptions, paginate } from './paginate'
 
 /**
  * Iterate through results of possibly inifite, paginated queries.
  *
- * @see {@link IterateOptions}
+ * @see {@link PaginateOptions}
+ * @see {@link paginate}
  * @example
  * ```
- * for await (const result of iterate({
+ * for await (const item of iterate({
  *   executeQuery: (limit, offset) =>
  *     query(
  *       client,
@@ -40,28 +15,13 @@ export interface IterateOptions<T> {
  *       { limit, offset }
  *     )
  * })) {
- *   console.info(result)
+ *   console.info(item)
  * }
  * ```
  */
-export async function* iterate<T>({
-  executeQuery,
-  interval = 0,
-  querySize = 100,
-  startingOffset = 0,
-}: IterateOptions<T>): AsyncGenerator<T> {
-  let response: GetByStatementResponse<T>
-  let length = 0
-  let offset = startingOffset
-
-  do {
-    const result = await executeQuery(querySize, offset)
-    response = Array.isArray(result) ? result[0] : result
-    if ((length = response.rval?.results?.length ?? 0))
-      for (const result of response.rval!.results!) yield result
-    if (length >= querySize) {
-      offset += querySize
-      await setTimeout(interval)
-    }
-  } while (length >= querySize)
+export async function* iterate<T>(
+  options: PaginateOptions<T>,
+): AsyncGenerator<T> {
+  for await (const { items } of paginate(options))
+    for (const item of items) yield item
 }
