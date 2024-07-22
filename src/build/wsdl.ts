b6e88c98cddfc6991e2da6fc52d1bcc50ed842fd
John Wright <jrite@protonmail.com>
2024-07-22 08:59:56 +0100

    chore: fix crawler typing

--- src/build/wsdl.ts
+++ src/build/wsdl.ts
@@ -1,48 +1,55 @@
 import axios from 'axios'
-import Crawler from 'crawler'
 import { mkdirp } from 'mkdirp'
 import { writeFile } from 'node:fs/promises'
 import { basePath, baseURL, origin } from './wsdl-path'
+import { CheerioAPI } from 'cheerio'
+;(async () => {
+  const { default: Crawler } = await import('crawler')
 
-const crawler = new Crawler({
-  maxConnections: 1,
-  callback: async (error, res, done) => {
-    if (error) {
-      console.error(error)
-      return done()
-    }
+  const crawler = new Crawler({
+    maxConnections: 1,
+    callback: async (error, res, done) => {
+      if (typeof done !== 'function')
+        throw new Error('`done` is not a function.')
 
-    const elements = res.$(`a[href$="?wsdl"]`)
+      if (error) {
+        console.error(error)
+        return done()
+      }
 
-    if (!elements.length) {
-      console.error(`Cannot find any WSDLs`)
-      return done()
-    }
+      const $: CheerioAPI = res.$
+      const elements = $(`a[href$="?wsdl"]`)
 
-    try {
-      await Promise.all(
-        elements.toArray().map(async (element) => {
-          const $element = res.$(element)
-          const title = $element.text()
-          const url = origin + $element.attr('href')
-          const versionRegExp = new RegExp(`${basePath}/([v0-9]+)/`)
-          const versionMatch = versionRegExp.exec(url)
-          if (!versionMatch) {
-            console.error(`Could not find a version in ${url}`)
-            return
-          }
-          const [, version] = versionMatch
-          await mkdirp(`src/wsdl/${version}`)
-          const response = await axios.get(url)
-          await writeFile(`src/wsdl/${version}/${title}.wsdl`, response.data)
-        }),
-      )
-    } catch (error) {
-      console.error(error)
-    }
+      if (!elements.length) {
+        console.error(`Cannot find any WSDLs`)
+        return done()
+      }
 
-    done()
-  },
-})
+      try {
+        await Promise.all(
+          elements.toArray().map(async (element) => {
+            const $element = res.$(element)
+            const title = $element.text()
+            const url = origin + $element.attr('href')
+            const versionRegExp = new RegExp(`${basePath}/([v0-9]+)/`)
+            const versionMatch = versionRegExp.exec(url)
+            if (!versionMatch) {
+              console.error(`Could not find a version in ${url}`)
+              return
+            }
+            const [, version] = versionMatch
+            await mkdirp(`src/wsdl/${version}`)
+            const response = await axios.get(url)
+            await writeFile(`src/wsdl/${version}/${title}.wsdl`, response.data)
+          }),
+        )
+      } catch (error) {
+        console.error(error)
+      }
 
-crawler.queue(baseURL)
+      done()
+    },
+  })
+
+  crawler.queue(baseURL)
+})()
